3dbdd4364c634ba83348f6bcd146b2b88947710c
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2022-06-24 16:59:48 +0930

    update

--- CDDS_Optimise/main.cpp
+++ CDDS_Optimise/main.cpp
@@ -140,7 +140,8 @@ int main(int argc, char* argv[])
         // check for critter-on-critter collisions
         for (int i = 0; i < CRITTER_COUNT; i++)
         {            
-            for (int j = 0; j < CRITTER_COUNT; j++){
+            for (int j = 0; j < CRITTER_COUNT; j++)
+            {
                 if (i == j || critters[i].IsDirty()) // note: the other critter (j) could be dirty - that's OK
                     continue;
                 // check every critter against every other critter
